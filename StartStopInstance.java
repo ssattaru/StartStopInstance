f87e9d47f90b5a8226f491503824ed28426b8295
Sunayana <68073508+ssattaru@users.noreply.github.com>
2020-07-13 14:49:08 -0400

    Add files via upload

---
+++ StartStopInstance.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aws_startstop;
+
+
+/**
+ *
+ * @author sunayanasattaru
+ */
+public class StartStopInstance {
+    public static void startInstance(String instance_id) {
+        final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+
+        DryRunSupportedRequest < StartInstancesRequest > dry_request =
+            () -> {
+            StartInstancesRequest request = new StartInstancesRequest()
+                .withInstanceIds(instance_id);
+
+            return request.getDryRunRequest();
+        };
+
+        DryRunResult dry_response = ec2.dryRun(dry_request);
+
+        if(!dry_response.isSuccessful()) {
+            System.out.printf(
+                "Failed dry run to start instance %s", instance_id);
+
+            throw dry_response.getDryRunResponse();
+        }
+
+        StartInstancesRequest request = new StartInstancesRequest()
+            .withInstanceIds(instance_id);
+
+        ec2.startInstances(request);
+
+        System.out.printf("Successfully started instance %s", instance_id);
+    }
+
+    public static void stopInstance(String instance_id)
+    {
+        final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+
+        DryRunSupportedRequest < StopInstancesRequest > dry_request =
+            () -> {
+            StopInstancesRequest request = new StopInstancesRequest()
+                .withInstanceIds(instance_id);
+
+            return request.getDryRunRequest();
+        };
+
+        DryRunResult dry_response = ec2.dryRun(dry_request);
+
+        if(!dry_response.isSuccessful()) {
+            System.out.printf(
+                "Failed dry run to stop instance %s", instance_id);
+            throw dry_response.getDryRunResponse();
+        }
+
+        StopInstancesRequest request = new StopInstancesRequest()
+            .withInstanceIds(instance_id);
+
+        ec2.stopInstances(request);
+
+        System.out.printf("Successfully stop instance %s", instance_id);
+    }
+
+    public static void main(String[] args) {
+        final String USAGE =
+            "To run this example, supply an instance id and start or stop\n" +
+            "Ex: StartStopInstance <instance-id> <start|stop>\n";
+
+        if (args.length != 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String instance_id = args[0];
+
+        boolean start;
+
+        if(args[1].equals("start")) {
+            start = true;
+        } 
+        else {
+            start = false;
+        }
+
+        if(start) {
+            startInstance(instance_id);
+        } 
+        else {
+            stopInstance(instance_id);
+        }
+    }
+}
